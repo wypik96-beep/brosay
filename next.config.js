e24bcd7701973c72a1b20f27ee9c157fe0ec4aa5
wypik96-beep <wypik96@gmail.com>
2025-10-22 18:43:20 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
